ab9e1feea97fe73c359e4fcd9dff22fddfda3bfb
aa2955 <aa2955@njit.edu>
2025-07-11 20:14:13 -0400

    API keys hidden

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDSA_VDbqRh81xYT2I_5YD-wBRSCKtkHas",
-  authDomain: "bhakti-jc.firebaseapp.com",
-  projectId: "bhakti-jc",
-  storageBucket: "bhakti-jc.firebasestorage.app",
-  messagingSenderId: "540605343227",
-  appId: "1:540605343227:web:875778a91ea0889648575e",
-  measurementId: "G-K68SD8G8BW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
